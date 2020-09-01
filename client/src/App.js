3e99c674b1964b581da1bf3b5e46f177a59754d3
Peeter Mäeots <maeots.peeter@gmail.com>
2020-09-01 16:12:00 +0300

    index page and data fetch implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TimeSince from './components/TimeSince';
+import { proxyUrl, rssUrl } from './utilities/constants';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // Initialize state
+  state = { 
+    articles: []
+  };
+
+  // Start RSS feed fetch after component mount
+  componentDidMount() {
+    this.getRssFeedData();
+  }
+
+  // Save RSS feed articles to state
+  getRssFeedData = () => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const xmlResponseTextItems = new window.DOMParser().parseFromString(request.responseText, "text/xml").querySelectorAll("item");
+        xmlResponseTextItems.forEach(el => {
+          const mediaContentElement = ([].slice.call(el.querySelectorAll('[url]'))).filter(function(a) { return a.tagName === 'media:content'});
+          const itemImageUrl = mediaContentElement.map(function(a) { return a.getAttribute('url') })[0];
+          const item = {
+            title: el.querySelector("title") !== null ? el.querySelector("title").innerHTML.toString() : '',
+            date: el.querySelector("pubDate") !== null ? el.querySelector("pubDate").innerHTML.toString() : '',
+            author: el.querySelector("author") !== null ? el.querySelector("author").innerHTML.toString() : '',
+            description: el.querySelector("description") !== null ? el.querySelector("description").innerHTML.toString() : '',
+            url: el.querySelector("link") !== null ? el.querySelector("link").innerHTML.toString() : '',
+            image: itemImageUrl ? itemImageUrl : 'img/penguin_placeholder_ls.jpg'
+          };
+          this.setState( prevState => ({
+            articles: [...prevState.articles, item]
+          }));
+        });
+      }
+    }
+    request.open("GET", proxyUrl + rssUrl, true);
+    request.send();
+  }
+  // Render app
+  render() {
+    const { articles } = this.state;
+    return (
+      <div className="App">
+        <header>
+          <div className="header">
+            <a href="/">
+              <img className="brand" src="img/lolo_news.png" alt=""></img>
+            </a>
+          </div>
+        </header>
+        <main className="main">
+          {articles.length ? (
+            // Render articles when fetch successful
+            <div>
+              <section className="archive">
+              {this.state.articles.map((item, i) => {
+                return (
+                  <article className="article" key={i}>
+                    <img className="card-image" src={item.image} alt="" onClick={() => this.toggleModal(item.url)}></img>
+                    <h2 onClick={() => this.toggleModal(item.url)}>
+                      { item.title }
+                    </h2>
+                    { item.author && <small className="article-author-small"><em>By { item.author }</em></small> }
+                    { item.date && <small><TimeSince date={ item.date } /></small> }
+                    <p onClick={() => this.toggleModal(item.url)}>
+                      { item.description }
+                    </p>
+                  </article>
+                );
+              })}
+              </section>
+            </div>
+          ) : (
+            // Show loader while fetching data
+            <div>
+              <img className="loader" src="img/loading.gif" alt=""></img>
+            </div>
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/utilities/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+  proxyUrl: 'https://silver-lining-proxy.herokuapp.com/',
+  rssUrl: 'https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss'
+}
+
+module.exports = constants;
