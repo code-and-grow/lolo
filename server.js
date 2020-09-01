8aa4b979528772367164f58d9ddca0ac633e6f9d
Peeter Mäeots <maeots.peeter@gmail.com>
2020-09-01 16:03:18 +0300

    added React app

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 const Mercury = require('@postlight/mercury-parser');
 const bodyParser = require('body-parser');
+const { resolve } = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req,res) => {
+  res.send("OK")
+})
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
