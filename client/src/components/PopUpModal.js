c5182accca9fecb218ef3e854477520c955a77be
Peeter Mäeots <maeots.peeter@gmail.com>
2020-09-01 16:30:59 +0300

    created get_parsed_articles route

--- client/src/components/PopUpModal.js
+++ client/src/components/PopUpModal.js
@@ -13,7 +13,7 @@ export default class PopUpModal extends React.Component {
   };
 
   componentDidMount() {
-    fetch('/get_article', {
+    fetch('/get_parsed_article', {
       method: 'POST',
       body: JSON.stringify({ url: this.props.openUrl }),
       headers:{ 'Content-Type': 'application/json' }
@@ -28,29 +28,29 @@ export default class PopUpModal extends React.Component {
     .catch(err => console.log(err));
   }
   
-render() {
-  return (
-    // Popup modal starts
-    <div className="modal">
-      <div className="modal_content">
-        <div className="close" onClick={this.handleClick}><b>&#88; Close</b></div>
-        {this.state.loading ? (
-          // Show loader while fetching article data
-          <div className="loader">
-            <img src="img/loading.gif" alt=""></img>
-          </div>
-        ) : (
-          // Render articles when fetch successful
-          <div>
-            <img src={ this.state.article.lead_image_url } alt=""></img>
-            <h1>{ this.state.article.title }</h1>
-            { this.state.article.author && <small className="article-author-small"><em>By { this.state.article.author }</em></small> }
-            { this.state.article.date_published && <small><TimeSince date={ this.state.article.date_published } /></small> }
-            <p>{ this.state.article.content }</p>
-          </div>
-        )}
+  render() {
+    return (
+      // Popup modal starts
+      <div className="modal">
+        <div className="modal_content">
+          <div className="close" onClick={this.handleClick}><b>&#88; Close</b></div>
+          {this.state.loading ? (
+            // Show loader while fetching article data
+            <div className="loader">
+              <img src="img/loading.gif" alt=""></img>
+            </div>
+          ) : (
+            // Render articles when fetch successful
+            <div>
+              <img src={ this.state.article.lead_image_url } alt=""></img>
+              <h1>{ this.state.article.title }</h1>
+              { this.state.article.author && <small className="article-author-small"><em>By { this.state.article.author }</em></small> }
+              { this.state.article.date_published && <small><TimeSince date={ this.state.article.date_published } /></small> }
+              <p>{ this.state.article.content }</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
- }
+    );
+  }
 }
--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require('express');
 const path = require('path');
 const Mercury = require('@postlight/mercury-parser');
 const bodyParser = require('body-parser');
-const { resolve } = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Create application/json parser
+const jsonParser = bodyParser.json()
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req,res) => {
-  res.send("OK")
-})
+// Un-cluttered article post request
+app.post('/get_parsed_article', jsonParser, (req, res) => {
+  try {
+    Mercury.parse(req.body.url, { contentType: 'text' }).then(response => res.status(200).send(response));
+  } catch (error) {
+    res.status(500).send({error: error});
+  }
+});
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+// Any request that doesn't match any above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
